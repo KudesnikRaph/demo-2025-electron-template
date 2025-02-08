a0a758392d87894f7ff0f59b721e612c2a3e7dad
KudesnikRaf <kolesnik.akk@gmail.com>
2025-02-08 14:32:58 +0300

    Upd indexes

--- src/main/index.js
+++ src/main/index.js
@@ -3,14 +3,51 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
-async function foo(event, data) {
+import connectDB from './db';
+
+async function getPartners() {
   try {
-    console.log(data)
-    dialog.showMessageBox({ message: 'message back' })
+    const response = await global.dbclient.query(`SELECT T1. *,
+      CASE WHEN sum(T2.production_quantity) > 300000 THEN 15
+      WHEN sum(T2.production_quantity) > 50000 THEN 10
+      WHEN sum(T2.production_quantity) > 10000 THEN 5
+      ELSE 0
+      END as discount
+      from partners as T1
+      LEFT JOIN sales as T2 on T1.id = T2.partner_id
+      GROUP BY T1.id`)
+      return response.rows
   } catch (e) {
-    dialog.showErrorBox('Ошибка', e)
+    console.log(e);
   }
 }
+
+async function createPartner(event, partner) {
+  const { type, name, ceo, email, phone, address, rating } = partner;
+
+  try {
+    await global.dbclient.query(`INSERT into partners (organization_type, name, ceo, email, phone, address, rating) values('${type}', '${name}', '${ceo}', '${email}', '${phone}', '${address}', ${rating})`)
+    dialog.showMessageBox({ message: 'Успех! Партнер создан' })
+  } catch (e) {
+    console.log(e)
+    dialog.showErrorBox('Ошибка', "Партнер с таким именем уже есть")
+  }
+}
+async function updatePartner(event, partner) {
+  const { id, type, name, ceo, email, phone, address, rating } = partner;
+
+  try {
+    await global.dbclient.query(`UPDATE partners
+      SET name = '${name}', organization_type = '${type}', ceo='${ceo}', email='${email}', phone='${phone}', address='${address}', rating='${rating}'
+      WHERE partners.id = ${id};`)
+    dialog.showMessageBox({ message: 'Успех! Данные обновлены' })
+    return;
+  } catch (e) {
+    dialog.showErrorBox('Невозможно создать пользователя', 'Такой пользователь уже есть')
+    return ('error')
+  }
+}
+
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -23,8 +60,9 @@ function createWindow() {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
     }
+    
   })
-
+  
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
@@ -41,10 +79,14 @@ function createWindow() {
   }
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   electronApp.setAppUserModelId('com.electron')
 
-  ipcMain.handle('sendSignal', foo)
+  global.dbclient = await connectDB();
+  
+  ipcMain.handle('getPartners', getPartners)
+  // ipcMain.handle('createPartner', createPartner)
+  // ipcMain.handle('updatePartner', updatePartner)
 
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
